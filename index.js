95f45b5c5234a7bb9d8f7e2be9671f8d7ac57fdd
bxzr1 <bxzr10@gmail.com>
2026-05-05 00:14:17 -0500

    fix color theme issues + tidy pages

--- index.js
+++ index.js
@@ -1,32 +1,35 @@
 const root = document.querySelector(':root');
-const colorSchemeToggle = document.getElementById('color-scheme-toggle');
+const themeToggle = document.getElementById('theme-toggle');
 
-const dark = '#203062';
-const medium = '#ED8A8D';
-const light = '#EFEBED';
+const darkBg = '#203062';
+const darkText = '#F89DA0';
+const darkAccent = '#EFEBED';
+
+const lightBg = '#FFFFFF';
+const lightText = '#FF4D6A';
+const lightAccent = '#4962ac';
 
 function isDarkMode() {
-    const bgColor = getComputedStyle(root).getPropertyValue('--main-bg-color');
-    if (bgColor === dark) return true;
+    const bgColor = getComputedStyle(root).getPropertyValue('--bg-color');
+    if (bgColor === darkBg) return true;
     else return false;
 }
 
 function toggleFromDarkMode(isDarkMode) {
-    // mode = true is dark mode while mode = false is light mode
     if (isDarkMode) {
         // light mode
-        root.style.setProperty('--main-bg-color', light)
-        root.style.setProperty('--main-text-color', dark)
-        root.style.setProperty('--main-text-accent', medium)
+        root.style.setProperty('--bg-color', lightBg)
+        root.style.setProperty('--text-color', lightText)
+        root.style.setProperty('--accent-color', lightAccent)
     } else {
         // dark mode
-        root.style.setProperty('--main-bg-color', dark)
-        root.style.setProperty('--main-text-color', medium)
-        root.style.setProperty('--main-text-accent', light)
+        root.style.setProperty('--bg-color', darkBg)
+        root.style.setProperty('--text-color', darkText)
+        root.style.setProperty('--accent-color', darkAccent)
     }
 }
 
-colorSchemeToggle.addEventListener('change', (e) => {
+themeToggle.addEventListener('change', (e) => {
     const currentMode = isDarkMode();
     toggleFromDarkMode(currentMode);
 })
